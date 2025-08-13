b8668973d3ec058e2e8fa87e648635307a31306c
David Balzer <dmbalzer@gmail.com>
2025-08-13 09:56:24 -0400

    reworking ray

--- src/ray.c
+++ src/ray.c
@@ -1,56 +1,54 @@
 #include "ray.h"
-#include "defs.h"
-#include <SDL3/SDL.h>
+#include <math.h>
 
-/* sdl.c */
-extern SDL_Renderer* renderer;
+static float VectorAngle(float x1, float y1, float x2, float y2) {
+    float result = 0.0f;
 
-/* player.c */
-extern Vector2 pos;
-extern Vector2 dir;
-extern Vector2 plane;
+    float dot = x1*x2 + y1*y2;
+    float det = x1*y2 - y1*x2;
 
-/* map.c */
-extern const int map[];
+    result = atan2f(det, dot);
 
-static float screen_dist = 0.0f;
+    return result;
+}
 
-Vector2 rays[SCREEN_W] = { 0 };
-bool ray_sides[SCREEN_W] = { 0 };
-float wall_strip_heights[SCREEN_W] = { 0 };
-int map_hit_nos[SCREEN_W] = { 0 };
+static float VectorLength(float x, float y) {
+    float result = sqrtf((x*x) + (y*y));
+    return result;
+}
 
-static void _ray_cast(Vector2 ray_dir, Vector2* ray, bool* h_hit, int* map_hit_no) {
+Ray ray_cast(float pos_x, float pos_y, float dir_x, float dir_y, int* map, int map_w, int map_h) {
+
 	/******************************************************
 	 * Horizontal Side Checks
 	 ******************************************************/
-	/* Horizontal Side Vector*/
-	Vector2 hside = { 0 };
-	hside.y = ray_dir.y < 0 ? (int)pos.y - pos.y : (int)pos.y - pos.y + 1;
-	hside.x = hside.y / tan(Vector2Angle((Vector2){1.0f, 0.0f}, ray_dir));
+	/* Calculate Horizontal Side Distances */
+	float hside_y = dir_y < 0 ? (int)pos_y - pos_y : (int)pos_y - pos_y + 1;
+	float hside_x = hside_y / tanf(VectorAngle(1.0f, 0.0f, dir_x, dir_y));
 
-	/* Horizontal Step Vector*/
+	/* Calculate Horizontal Step Distances */
 	Vector2 hstep = { 0 };
-	hstep.y = ray_dir.y < 0 ? -1 : 1;
-	hstep.x = hstep.y / tan(Vector2Angle((Vector2){1.0f, 0.0f}, ray_dir));
+	float hstep_y = dir_y < 0 ? -1 : 1;
+	float hstep_x = hstep_y / tanf(VectorAngle(1.0f, 0.0f, dir_x, dir_y));
 
-	bool hit = false;
-	Vector2 hray = hside;
-	int h_map_no = 0;
+	int hit = 0;
+	float hray_x = hside_y;
+	float hray_y = hside_x;
+	int h_map_hit_no = 0;
+
 	while ( !hit ) {
-		int x = (int)(pos.x + hray.x);
-		if ( x >= MAP_W ) x = MAP_W - 1;
-		if ( x < 0 ) x = 0;
-		int y = ray_dir.y < 0 ? (int)(pos.y + hray.y) - 1 : (int)(pos.y + hray.y);
-		if ( y >= MAP_H ) y = MAP_H - 1;
-		if ( y < 0 ) y = 0;
-		if ( map[ x + y * MAP_W ] != 0 ) {
+
+		int x = (int)(pos_x + hray_x);
+		int y = dir_y < 0 ? (int)(pos_y + hray_y) - 1 : (int)(pos_y + hray_y);
+
+		if ( map[ x + y * map_w ] != 0 ) {
 			
-			h_map_no = map[ x + y * MAP_W ];
+			h_map_hit_no = map[ x + y * map_w ];
 			break;
 		}
-		hray = Vector2Add(hray, hstep);
-	}
+
+		hray_x += hstep_x;
+		hray_y += hstep_y;
 
 	/******************************************************
 	 * Vertical Side Checks
@@ -83,46 +81,35 @@ static void _ray_cast(Vector2 ray_dir, Vector2* ray, bool* h_hit, int* map_hit_n
 		}
 		vray = Vector2Add(vray, vstep);
 	}
+
+	Ray ray = { 0 };
 	
 	*h_hit = (Vector2Length(hray) < Vector2Length(vray));
 	*ray = *h_hit ? hray : vray;
 	*map_hit_no = *h_hit ? h_map_no : v_map_no;
 }
 
-float _get_perp_dist(Vector2 cam_plane, Vector2 ray, bool h_hit) {
-	float angle = Vector2Angle(ray, dir);
-	return Vector2Length(ray) * cosf(angle);
-}
-
-void ray_update(void) {
-	if ( screen_dist == 0.0f ) {
-		float half_fov = Vector2Angle(dir, Vector2Add(dir, plane));
-		screen_dist = (SCREEN_W / 2) / tanf(half_fov);
-	}
-
-    for ( int i = 0; i < SCREEN_W; i++ ) {
-		float x = 2 * i / (float)SCREEN_W - 1;
-		Vector2 cam_plane = Vector2Scale(plane, x);
-		Vector2 ray_dir = (Vector2){
-			.x = dir.x + cam_plane.x,
-			.y = dir.y + cam_plane.y
-		};
-
-        _ray_cast(ray_dir, &rays[i], &ray_sides[i], &map_hit_nos[i]);
-		float perp_dist = _get_perp_dist(cam_plane, rays[i], ray_sides[i]);
-		wall_strip_heights[i] = 1 / perp_dist * screen_dist;
-    }
-}
-
-void ray_draw(void) {
-	for ( int i = 0; i < SCREEN_W; i++ ) {
-
-		/* Draw Ray */
-		SDL_SetRenderDrawColor(renderer, GREEN);
-		SDL_RenderLine(renderer,
-			pos.x * TILE_SIZE,
-			pos.y * TILE_SIZE,
-			(pos.x + rays[i].x) * TILE_SIZE,
-			(pos.y + rays[i].y) * TILE_SIZE);
-	}
-}
+// float _get_perp_dist(Vector2 cam_plane, Vector2 ray, bool h_hit) {
+// 	float angle = Vector2Angle(ray, dir);
+// 	return Vector2Length(ray) * cosf(angle);
+// }
+// 
+// void ray_update(void) {
+// 	if ( screen_dist == 0.0f ) {
+// 		float half_fov = Vector2Angle(dir, Vector2Add(dir, plane));
+// 		screen_dist = (SCREEN_W / 2) / tanf(half_fov);
+// 	}
+// 
+//     for ( int i = 0; i < SCREEN_W; i++ ) {
+// 		float x = 2 * i / (float)SCREEN_W - 1;
+// 		Vector2 cam_plane = Vector2Scale(plane, x);
+// 		Vector2 ray_dir = (Vector2){
+// 			.x = dir.x + cam_plane.x,
+// 			.y = dir.y + cam_plane.y
+// 		};
+// 
+//         _ray_cast(ray_dir, &rays[i], &ray_sides[i], &map_hit_nos[i]);
+// 		float perp_dist = _get_perp_dist(cam_plane, rays[i], ray_sides[i]);
+// 		wall_strip_heights[i] = 1 / perp_dist * screen_dist;
+//     }
+// }
--- src/ray.h
+++ src/ray.h
@@ -1,9 +1,16 @@
 #ifndef RAY_INCLUDED
 #define RAY_INCLUDED
 
-#include "raymath.h"
+typedef struct _ray Ray;
+struct _ray {
+	float x;		// Vector component x
+	float y;		// Vector component y
+	float x_perp;	// Perpendicular Vector component x
+	float y_perp;	// Perpendicular Vector component y
+	int horiz_hit;	// Was hit on horizontal side of wall
+	int map_hit_idx;// Map index of hit
+};
 
-void ray_update(void);
-void ray_draw(void);
+Ray ray_cast(float xpos, float ypos, float xdir, float ydir, int* map, int map_w, int map_h);
 
 #endif // RAY_INCLUDED
